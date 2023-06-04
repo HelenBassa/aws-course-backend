e32f464c8a3027465a261b7dec4278a2c0de3cd2
HelenBassa <helenbassa@gmail.com>
2023-06-04 15:52:52 +0300

    First API with AWS API Gateway and AWS Lambda

---
+++ product-service/src/handlers/constants.ts
@@ -0,0 +1,52 @@
+/*
+    id -  uuid (Primary key)
+    title - text, not null
+    description - text
+    price - integer
+
+*/
+
+export const PRODUCTS = [
+  {
+    id: "1",
+    title: "Trek",
+    description:
+      "Trek is a bicycle brand that had humble beginnings, starting its life in a barn in 1976. They seemed to have done alright since then, considering it was a Trek 5500 that Lance Armstrong rode during his first Tour de France win in 1999. Almost all of his consecutive wins after that were also on the backs of Trek bikes. In 1998, Trek created its Advanced Concepts Group, which was a pool of engineers and technicians focused on bike technology development. With all that brainpower built into Trek bikes, it’s not hard to see why so many cyclists consider this the best bike brand in the world. ",
+    price: "5000",
+  },
+  {
+    id: "2",
+    title: "Specialized",
+    description:
+      "Bike brand Specialized was the first manufacturer to bring a mass-produced mountain bike, the Stumpjumper, to the world, in 1981. Built by riders for riders, Specialized’s bikes are backed by the motto, “If you ride, we’re for you”. It’s not just a saying—when the brand saw children struggling to focus in school they started supporting the Outride program, which is designed to promote better health (and thus concentration) through cycling. It looks like Specialized is to blame for getting whole generations of kids into mountain biking… and we thank them for it.",
+    price: "4000",
+  },
+  {
+    id: "3",
+    title: "Cannondale",
+    description:
+      "Oddly enough, bike brand Cannondale might have first gained worldwide notice not specifically because of their bicycles, but because of The Bugger, the very first bicycle trailer to hit the market. That doesn’t mean they don’t now make world-class rides. The brand’s first bicycle was released in the early 1980s, and they went on to be one of the first to start making frames out of carbon in 2004. Since then, Cannondale bikes have made regular appearances at Le Tour de France, including some podium-topping performances. Now they’re even moving into the ebike market, designing electric rides to rival the best electric scooters.",
+    price: "3000",
+  },
+  {
+    id: "4",
+    title: "Santa Cruz",
+    description:
+      "Santa Cruz is a niche bicycle brand, going all-in on building bikes for hardcore mountain bikers. Started in a garage in Santa Cruz, California in 1993, the brand has always maintained a laser-sharp focus on the mountain bike market. To be honest, Santa Cruz bikes aren’t necessarily going to be for everyone, especially downhill beginners, but if you love the rush of hurling yourself down the side of a mountain on two sturdy wheels, you should definitely give this bike brand a look. ",
+    price: "2000",
+  },
+  {
+    id: "5",
+    title: "Scott",
+    description:
+      "Believe it or not, Scott came to their world class range of bikes via a skiing pole. Engineer Ed Scott began his company back in 1958 as a maker of aluminum ski poles, and the sports company has been innovating ever since. In 1992, Scott brought the first full suspension mountain bike to the market, and followed it up in 1998 with the G-Zero, the lightest full-suspension mountain bike which set the bar for mountain rides. The brand is still continuing to expand and innovate, adding road, gravel, cyclocross, hybrid, and kids’ bikes to their line-up.",
+    price: "1000",
+  },
+  {
+    id: "6",
+    title: "Giant",
+    description:
+      "Giant Bicycles is appropriately named. Founded in 1973, Giant is the biggest bicycle brand in the world, having produced more than 6.6 million bikes (as of 2014). As you’d expect from a brand with those kinds of numbers, they’re not exactly shooting for niche markets. Giant builds bicycles for just about every kind of rider, and builds them to last. And because they move such volume, Giant is able to offer bikes at budget-friendly prices, adding more adherents to the biking habit around the world.",
+    price: "500",
+  },
+];
