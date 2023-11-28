c83c7c6a7daf70245a29a822c9282fae3b6c7787
HelenBassa <helenbassa@gmail.com>
2023-11-28 21:46:29 +0300

    fix: task-4 - add transaction based creation of product

--- product-service/src/handlers/createProduct.ts
+++ product-service/src/handlers/createProduct.ts
@@ -1,5 +1,9 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument, GetCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  DynamoDBDocument,
+  GetCommand,
+  TransactWriteCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 import { buildResponse } from "./libs/utils.js";
 import { v4 as uuid } from "uuid";
@@ -46,21 +50,48 @@ export const handler = async (event: any) => {
         },
       };
 
-      docClient.put(StockParams, function (err: any, data: any) {
-        if (err) {
-          console.log("Error Stock", err);
-        } else {
-          console.log("Success Stock", data);
-        }
-      });
+      // docClient.put(StockParams, function (err: any, data: any) {
+      //   if (err) {
+      //     console.log("Error Stock", err);
+      //   } else {
+      //     console.log("Success Stock", data);
+      //   }
+      // });
 
-      docClient.put(ProductParams, function (err: any, data: any) {
-        if (err) {
-          console.log("Error Product", err);
-        } else {
-          console.log("Success Product", data);
-        }
-      });
+      // docClient.put(ProductParams, function (err: any, data: any) {
+      //   if (err) {
+      //     console.log("Error Product", err);
+      //   } else {
+      //     console.log("Success Product", data);
+      //   }
+      // });
+
+      await docClient.send(
+        new TransactWriteCommand({
+          TransactItems: [
+            {
+              Put: {
+                TableName: "Product",
+                Item: {
+                  id: product.id,
+                  title: product.title,
+                  description: product.description,
+                  price: product.price,
+                },
+              },
+            },
+            {
+              Put: {
+                TableName: "Stock",
+                Item: {
+                  product_id: product.id,
+                  count: product.count,
+                },
+              },
+            },
+          ],
+        })
+      );
 
       const command = new GetCommand({
         TableName: "Product",
