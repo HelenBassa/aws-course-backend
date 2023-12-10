926c7ac05e2f44747393acd9369de744ecf31585
HelenBassa <helenbassa@gmail.com>
2023-12-11 00:30:34 +0300

    task--7: implement authorization-service

--- authorization-service/bin/authorization-service.ts
+++ authorization-service/bin/authorization-service.ts
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AuthorizationServiceStack } from '../lib/authorization-service-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { config } from "dotenv";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+config();
+
+const { AUTHORIZATION_SERVICE_AWS_REGION } = process.env;
 
 const app = new cdk.App();
-new AuthorizationServiceStack(app, 'AuthorizationServiceStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stack = new cdk.Stack(app, "AuthorizationServiceStack", {
+  env: { region: AUTHORIZATION_SERVICE_AWS_REGION! },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const environment = {
+  HelenBassa: process.env.HelenBassa!,
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const basicAuthorizer = new NodejsFunction(stack, "basicAuthorizerLambda", {
+  runtime: lambda.Runtime.NODEJS_18_X,
+  functionName: "basicAuthorizer",
+  entry: "src/handlers/basicAuthorizer.ts",
+  environment,
 });
