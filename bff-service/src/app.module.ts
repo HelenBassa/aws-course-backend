6b3e2e0e8cf150cb0125610e54b0030d3cdf9d0c
HelenBassa <helenbassa@gmail.com>
2024-01-07 22:15:21 +0300

    feat: add bff

---
+++ bff-service/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    CacheModule.register(),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
