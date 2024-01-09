f322a7008e0e1d4c3241a72fcc45ea0d3c8bd647
HelenBassa <helenbassa@gmail.com>
2024-01-09 22:53:40 +0300

    fix: fix url

--- bff-service/src/app.service.ts
+++ bff-service/src/app.service.ts
@@ -19,25 +19,34 @@ export class AppService {
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
   async getResponse(): Promise<IResponse> {
-    const { originalUrl, method, body } = this.request;
-    console.log('originalUrl', originalUrl);
-    console.log('method', method);
-    console.log('body', body);
+    const { originalUrl, method, body, headers } = this.request;
+    console.log({ originalUrl });
+    console.log({ method });
+    console.log({ body });
+    console.log({ headers });
 
     const [, recipient, ...path] = originalUrl.split('/');
-    const recipientURL = process.env[recipient];
-    console.log('recipientURL', recipientURL);
+    console.log({ recipient });
+    console.log({ ...path });
 
-    if (originalUrl === 'products' && method === 'GET') {
+    const recipientURL = process.env[recipient];
+    console.log({ recipientURL });
+
+    if (originalUrl === '/products' && method === 'GET') {
       const data: IResponse['data'] = await this.cacheManager.get(PRODUCTS);
       console.log('FROM CACHE:', data);
       if (data) return { status: 200, data };
     }
 
     if (recipientURL) {
+      const url = `${recipientURL}/${path}`;
+      console.log({ url });
       const axiosConfig: AxiosRequestConfig = {
         method: method,
-        url: `${recipientURL}/${path.join('/')}`,
+        url: url.replace(/\/$/, ''),
+        headers: headers.authorization
+          ? { Authorization: headers.authorization }
+          : {},
         ...(Object.keys(body || {}).length > 0 && { data: body }),
       };
 
@@ -46,7 +55,7 @@ export class AppService {
       try {
         const { status, data } = await axios(axiosConfig);
         console.log('response from recipient', data);
-        if (originalUrl === 'products' && method === 'GET') {
+        if (originalUrl === '/products' && method === 'GET') {
           const isTimerSeted = await this.cacheManager.get(PRODUCTS);
           if (!isTimerSeted) console.log('SET TO CACHE');
           await this.cacheManager.set(PRODUCTS, data, 120);
